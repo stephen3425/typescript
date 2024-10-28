2daa2a8f21d9d68110de63e8a24a163d78c9e662
Vincent Tejas V <vncnttejas@users.noreply.github.com>
2024-10-28 21:24:55 +0530

    Day 2 exercise

---
+++ anagram.ts
@@ -0,0 +1,10 @@
+function checkIfAnagram(word1: string, word2: string) {
+
+}
+
+checkIfAnagram("dad", "add"); // true
+checkIfAnagram("boy", "oby"); // true
+checkIfAnagram("boy", "yob"); // true
+checkIfAnagram("boy", "toy"); // false
+checkIfAnagram("bell", "bel"); // false
+checkIfAnagram("bell", "lebl"); // false
